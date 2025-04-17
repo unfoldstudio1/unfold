f56a82669d5320bef5df4d6d5f9de584282d62ed
unfoldstudio1 <unfold.apps@gmail.com>
2025-04-18 02:22:28 +0300

    test2

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
+      <Text>Hello Worlddddd</Text>
     </View>
   );
 }
